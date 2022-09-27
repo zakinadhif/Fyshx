067b7ed0d78de771cc68f5c1d3974e931f826d89
zakinadhif <zaki.nadhif45@gmail.com>
2022-09-27 21:46:08 +0700

    Draw the first circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,48 @@
 #include <SFML/Graphics.hpp>
 
+struct Body {
+  sf::Vector2f position;
+  float rotation;
+  float radius;
+};
+
+void renderBodies(sf::RenderTarget& target, const std::vector<Body>& bodies)
+{
+  sf::CircleShape circle;
+
+  for (const auto& body : bodies) {
+    circle.setPosition(body.position);
+    circle.setRotation(body.rotation);
+    circle.setRadius(body.radius);
+
+    target.draw(circle);
+  }
+}
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+  auto window = sf::RenderWindow{ { 400u, 400u }, "Physics Simulation" };
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
+  std::vector<Body> bodies;
+  bodies.emplace_back(Body {
+    .position = {200.f, 200.f},
+    .rotation = 0.0f,
+    .radius = 9.0f,
+  });
+
+  while (window.isOpen())
+  {
+    for (auto event = sf::Event{}; window.pollEvent(event);)
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
     }
+
+    window.clear();
+    renderBodies(window, bodies);
+    window.display();
+  }
 }
